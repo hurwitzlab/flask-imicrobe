d0576fa47810cc564039a0149d42fe0439533f64
Joshua Lynch <jklynch@email.arizona.edu>
2017-07-11 05:45:07 +0100

    add SQLALCHEMY_POOL_RECYCLE

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = 60  # should be less than DB connection timeout
     SQLALCHEMY_DATABASE_URI = os.environ.get('IMICROBE_DB_URI')
 
     def init_app(self, app):
@@ -15,7 +16,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
 
 
 class ProductionConfig(Config):

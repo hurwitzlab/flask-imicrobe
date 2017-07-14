7be390b6c7352cbdb5117c21cbbec605fa38f8d3
Joshua Lynch <jklynch@email.arizona.edu>
2017-07-14 17:00:28 +0100

    add testing configuration

--- config.py
+++ config.py
@@ -18,11 +18,16 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 
+class TestingConfig(Config):
+    DEBUG = True
+
+
 class ProductionConfig(Config):
     DEBUG = False
 
 
 configs = {
     'development': DevelopmentConfig(),
+    'testing': TestingConfig(),
     'production': ProductionConfig()
 }

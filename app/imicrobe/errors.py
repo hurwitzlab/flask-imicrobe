14fe4d938114235f8cceadb54ec78a9e4a73cf12
Joshua Lynch <jklynch@email.arizona.edu>
2017-07-14 17:04:28 +0100

    implement 403 and 404 error handlers

---
+++ app/imicrobe/errors.py
@@ -0,0 +1,17 @@
+from flask import jsonify
+
+from . import imicrobe
+
+
+@imicrobe.app_errorhandler(403)
+def forbidden(message):
+    response = jsonify({'error': 'forbidden', 'message': message})
+    response.status_code = 403
+    return response
+
+
+@imicrobe.app_errorhandler(404)
+def not_found(message):
+    response = jsonify({'error': 'Not found', 'message': message})
+    response.status_code = 404
+    return response

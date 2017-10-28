382d3ace87ab8c0fc42a86211c74da7e4415a5fa
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-28 16:50:42 -0400

    message variable is not jsonifying

--- app/imicrobe/errors.py
+++ app/imicrobe/errors.py
@@ -5,13 +5,15 @@ from . import imicrobe
 
 @imicrobe.app_errorhandler(403)
 def forbidden(message):
-    response = jsonify({'error': 'forbidden', 'message': message})
+    ##response = jsonify({'error': 'forbidden', 'message': message})
+    response = jsonify({'error': 'forbidden'})
     response.status_code = 403
     return response
 
 
 @imicrobe.app_errorhandler(404)
 def not_found(message):
-    response = jsonify({'error': 'Not found', 'message': message})
+    ##response = jsonify({'error': 'Not found', 'message': message})
+    response = jsonify({'error': 'Not found'})
     response.status_code = 404
     return response

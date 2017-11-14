19816bde9a9d7f9a6114e5a35fc0f985906d1966
Joshua Lynch <jklynch@email.arizona.edu>
2017-11-13 21:59:38 -0500

    remove import Project and other unused code

--- manage.py
+++ manage.py
@@ -3,24 +3,13 @@ import os
 
 import pytest
 
-from flask_script import Manager, Shell
+from flask_script import Manager
 
 from app import create_app, db
-from app.models import Project
 
 
 app = create_app(os.environ.get('IMICROBE_FLASK_CONFIG', default='development'))
 manager = Manager(app)
-
-
-def make_shell_context():
-    # TODO: need all model classes here? should be programmatic
-    return {
-        'app': app, 'db': db, 'Project': Project
-    }
-
-
-manager.add_command('shell', Shell(make_context=make_shell_context()))
 
 
 @manager.command

b3041c32ae76d18196455d90bf0f55326eb0518d
Joshua Lynch <jklynch@email.arizona.edu>
2018-02-08 17:50:05 -0500

    knock a few more lists out of the sample view

--- app/model_view.py
+++ app/model_view.py
@@ -4,6 +4,9 @@ from flask_admin.contrib.sqla import ModelView
 class iMicrobeModelView(ModelView):
     column_display_pk = True
     form_excluded_columns = [
+        'assembly_list',
+        'combined_assembly_list',
+        'centrifuge_list',
         'protein_list',
         'uproc_kegg_result_list',
         'uproc_pfam_result_list']  # this is just for Sample lazy!

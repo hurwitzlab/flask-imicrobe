d34ec06642caa5ecf418ce5b69b0bd5f3001d397
Joshua Lynch <jklynch@email.arizona.edu>
2018-02-08 17:44:33 -0500

    do not display protein list

--- app/model_view.py
+++ app/model_view.py
@@ -3,4 +3,7 @@ from flask_admin.contrib.sqla import ModelView
 
 class iMicrobeModelView(ModelView):
     column_display_pk = True
-    form_excluded_columns = ['uproc_kegg_result_list', 'uproc_pfam_result_list']  # this is just for Sample lazy!
+    form_excluded_columns = [
+        'protein_list',
+        'uproc_kegg_result_list',
+        'uproc_pfam_result_list']  # this is just for Sample lazy!

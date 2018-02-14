d093b3dc8e72828340eb9be5cbb7e830c67770ba
Joshua Lynch <jklynch@email.arizona.edu>
2018-02-14 12:59:42 -0500

    change models_class to model_class

--- app/__init__.py
+++ app/__init__.py
@@ -38,13 +38,13 @@ def create_app(config_name):
         template_mode='bootstrap3',
         url=app_.ADMIN_URL)
 
-    for models_class in models.__dict__.values():
-        if isinstance(models_class, type) and models_class.__module__ == models.__name__:
-            print('found model class "{}"'.format(models_class.__name__))
-            if models_class.__name__ == 'Sample':
-                view = SampleView(models_class, db.session)
+    for model_class in models.__dict__.values():
+        if isinstance(model_class, type) and model_class.__module__ == models.__name__:
+            print('found model class "{}"'.format(model_class.__name__))
+            if model_class.__name__ == 'Sample':
+                view = SampleView(model_class, db.session)
             else:
-                view = iMicrobeModelView(models_class, db.session)
+                view = iMicrobeModelView(model_class, db.session)
             admin.add_view(view)
         else:
             pass
